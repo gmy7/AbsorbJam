b23bfc357dd62788f965687552574c3606388dfc
gmy7 <GregM.Young1@gmail.com>
2022-08-15 12:21:15 -0400

    v1

--- Assets/Scripts/Magma.cs
+++ Assets/Scripts/Magma.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class Magma : MonoBehaviour
 {
-    private Collider2D col2D;
-    private Animator animator;
+    private CircleCollider2D col2D;
     private void Awake()
     {
-        col2D = GetComponent<Collider2D>();
-        animator = GetComponent<Animator>();
+        col2D = GetComponent<CircleCollider2D>();
     }
+    private void Update()
+    {
+        if(col2D.radius > 1.38) { return; }
+        col2D.radius += 2 * Time.deltaTime;
+    }
+    public void FinishMagma()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.CompareTag("EnemyProjectile"))
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Player player = collision.gameObject.GetComponent<Player>();
+                player.TakeHit(true, Crystal.CrystalType.Red);
+            }
+        }
+        if (gameObject.CompareTag("PlayerProjectile"))
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                collision.gameObject.GetComponent<Enemy>().TakeHit();
+            }
+        }
+    }
+
 }
